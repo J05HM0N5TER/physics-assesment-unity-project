48b122ed439a9fc51eb1406556232c09a3ab6d8d
Joshua Francis <joshua.francis@live.com.au>
2020-04-27 12:32:45 +1000

    Rigidbody trigger working

--- Assets/Scripts/Ragdoll.cs
+++ Assets/Scripts/Ragdoll.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Ragdoll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private Animator animator = null;
+	public List<Rigidbody> rigidbodies = new List<Rigidbody>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+	public bool RagdollOn 
+	{ 
+		get
+		{ 
+			return! animator.enabled; 
+		}
+		set
+		{
+			animator.enabled = !value;
+			foreach(Rigidbody r in rigidbodies)
+				r.isKinematic = !value;
+		}
+	}
+	
+	// Use this for initialisation
+	void Start() 
+	{
+		animator = GetComponent<Animator>();
+		Rigidbody[] temp = GetComponentsInChildren<Rigidbody>();
+		for (int i = 0; i < temp.Length; i++)
+		{
+			rigidbodies.Add(temp[i]);
+		}
+
+		foreach(Rigidbody r in rigidbodies)
+			r.isKinematic = true;
+	}
 }
