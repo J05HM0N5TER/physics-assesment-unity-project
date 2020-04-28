5ad110e4431b87b2bb818b4d9faf3a8c6ddd9d22
Joshua Francis <joshua.francis@live.com.au>
2020-04-29 09:25:59 +1000

    Added script to control animation
    The script is meant to change between the walk animation and pose based
    on how fast it is moving. This is currently not working correctly.

---
+++ Assets/Scripts/RagdollAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RagdollAnimation : MonoBehaviour
+{
+    Vector3 previousPostion;
+    Animator ani = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        previousPostion = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ani.SetFloat("speed", (transform.position - previousPostion).magnitude);
+        previousPostion = transform.position;
+    }
+}
