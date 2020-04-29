38bb06540f4cd90f612e19550dedca8e97fe9877
Joshua Francis <joshua.francis@live.com.au>
2020-04-29 10:55:43 +1000

    Added trapdoor for ragdoll to fall though
    Fixed most of the broken things so that it seems to work correctly now.
    I just need to add some kind of raycast.

---
+++ Assets/Scripts/buttonController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class buttonController : MonoBehaviour
+{
+    public Rigidbody platform = null;
+    public Ragdoll ragdoll = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ragdoll.RagdollOn = true;
+        platform.isKinematic = false;
+    }
+}
